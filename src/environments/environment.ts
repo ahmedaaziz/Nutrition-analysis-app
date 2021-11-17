0be2c3ec0caf6fc5f358ae3b942aa27b1bec3f61
ahmed0abdelaziz@gmail.com <ahmed0abdelaziz@gmail.com>
2021-11-17 18:51:06 +0200

    fix http req blocked issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiRoot : 'http://api.edamam.com/api/nutrition-data?',
+  apiRoot : 'https://api.edamam.com/api/nutrition-data?',
   app_id:'4457f8c8',
   app_key:'60c24a84cae168dbf3793f13388a5014',
   nutrition_type:'cooking'
